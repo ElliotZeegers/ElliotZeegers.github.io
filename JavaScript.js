11e2e7082a5259cd3b7e75d4a0259b3f21a8f496
ElliotZeegers <0336362@student.rocvantwente.nl>
2024-03-01 09:33:31 +0100

    First commit
    eerste commit met files

---
+++ JavaScript.js
@@ -0,0 +1,76 @@
+const body = document.getElementById("kleur");
+const nav = document.getElementById("navigatie");
+const navText = document.getElementById("navlinktext");
+const balk = document.getElementById("balk");
+const iconColor = document.getElementById("iconColor");
+const extra = document.getElementsByClassName("card");;
+let checker = false;
+console.log("no more da");
+
+function colorChanger(){
+    if(checker == false) {
+        body.style.backgroundColor = "#262626";
+        body.style.color = "white";
+        nav.style.backgroundColor = "#F8F9FA";
+        navText.style.color = "black";
+        balk.style.backgroundColor = "#1a1a1a";
+        balk.style.color = "white";
+        iconColor.style.color = "black";
+        checker = true;
+    } else {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+        nav.style.backgroundColor = "#262626";
+        navText.style.color = "white";
+        balk.style.backgroundColor = "#F8F9FA";
+        balk.style.color = "black";
+        iconColor.style.color = "white";
+        checker = false;
+    }
+}
+
+function colorChangerExtra(){
+    if(checker == false) {
+        body.style.backgroundColor = "#262626";
+        body.style.color = "white";
+        nav.style.backgroundColor = "#F8F9FA";
+        navText.style.color = "black";
+        balk.style.backgroundColor = "#1a1a1a";
+        balk.style.color = "white";
+        iconColor.style.color = "black";
+        for (let i = 0; i < extra.length; i++) {
+            extra[i].style.backgroundColor = "#595959";
+        }
+        checker = true;
+    } else {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+        nav.style.backgroundColor = "#262626";
+        navText.style.color = "white";
+        balk.style.backgroundColor = "#F8F9FA";
+        balk.style.color = "black";
+        iconColor.style.color = "white";
+        for (let i = 0; i < extra.length; i++) {
+            extra[i].style.backgroundColor = "white";
+        }
+        checker = false;
+    }
+}
+
+function colorChangerIndex(){
+    if(checker == false) {
+        body.style.backgroundColor = "#262626";
+        body.style.color = "white";
+        nav.style.backgroundColor = "#F8F9FA";
+        navText.style.color = "black";
+        iconColor.style.color = "black";
+        checker = true;
+    } else {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+        nav.style.backgroundColor = "#262626";
+        navText.style.color = "white";
+        iconColor.style.color = "white";
+        checker = false;
+    }
+}
